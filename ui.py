539e86841366a70ac2623e22738b4b632311eba6
emily_wang_0_0 <51404834+emilywz@users.noreply.github.com>
2020-03-10 14:42:29 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,7 +1,6 @@
 from bll import GameCoreController
 from model import DirectionModel
 
-# 导入系统
 import os
 
 
@@ -14,39 +13,31 @@ class GameConsoleView:
         self.__update()
 
     def _start(self):
-        # 产生两个数字
         self.__controller.generate_random_number()
         self.__controller.generate_random_number()
-        # 绘制界面
         self.__draw_list_map()
 
     def __update(self):
-        # 循环
         while True:
-            # 判断玩家的输入--->移动地图
             self.__move_list_map()
 
-            # 产生新数字
             self.__controller.generate_random_number()
 
-            # 绘制界面
             self.__draw_list_map()
 
-            # 游戏结束判断-->提示
             if self.__controller.is_game_over():
-                print("游戏结束")
+                print("GameOver")
                 break
 
     def __draw_list_map(self):
-        # 清空控制台
         os.system("clear")
-        for line in self.__controller.list_map:  # 行
-            for item in line:  # 列
+        for line in self.__controller.list_map:  
+            for item in line:
                 print(item, end=" ")
             print()
 
     def __move_list_map(self):
-        dir = input("请输入方向（wsda）：")
+        dir = input("Plear input your direction（wsda）：")
 
         dict_dir = {
             "w": DirectionModel.UP,
@@ -58,7 +49,7 @@ class GameConsoleView:
         if dir in dict_dir:
             self.__controller.move(dict_dir[dir])
 
-# -------------------------------------测试--------------------------
+# -------------------------------------test--------------------------
 # if __name__=="__main__":
 #     view=GameConsoleView()
 #     view.main()
